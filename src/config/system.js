aefe136c9065eb9cee30ef5f6f5c687d5ca405c5
Rakesh Kumar Sah Gupta <rakeshsoftware206@gmail.com>
2019-12-09 12:56:59 +0545

    Project Added
    Project Added

---
+++ src/config/system.js
@@ -0,0 +1,8 @@
+const system = {
+    appName: 'TSHIRTS',
+    serverBaseUrl: 'https://backendapi.turing.com',
+    imageBaseUrl: 'https://backendapi.turing.com/images/products/',
+    stripeToken: 'pk_test_NcwpaplBCuTL6I0THD44heRe'
+};
+
+export default system;
